ae20902bbfda98336501efe6a6134180d3643ed7
Abdullah <abdallahmosbah25@gmail.com>
2023-09-08 11:48:30 -0700

    first 6

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    my_new_string = ""  
+    for i in my_string:
+        if i == 'C' or i == 'c':
+            continue
+        else:
+            my_new_string += i
+    return my_new_string
