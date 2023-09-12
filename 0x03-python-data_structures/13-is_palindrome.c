d7b516722273b5cab691a82bef4c6b40cae79067
Abdullah <abdallahmosbah25@gmail.com>
2023-09-11 22:38:15 -0700

    is

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
-
 /**
  * is_palindrome - checks if a singly linked list is a palindrome
  * @head: pointer to the head of the linked list
@@ -9,55 +8,43 @@
  */
 int is_palindrome(listint_t **head)
 {
-    listint_t *slow = *head, *fast = *head;
-    listint_t *prev = NULL, *temp, *second_half = NULL;
-    int is_palindrome = 1; // Assume it's a palindrome by default
+	listint_t *slow = *head, *fast = *head;
+	listint_t *prev = NULL, *temp, *second_half = NULL;
+	int is_palindrome = 1;
 
-    if (*head == NULL || (*head)->next == NULL)
-        return (1); // Empty list or single element is a palindrome
+	if (*head == NULL || (*head)->next == NULL)
+	return (1);
+	while (fast && fast->next)
+	{
+		fast = fast->next->next;
+		temp = slow;
+		slow = slow->next;
+	}
+	if (fast)
+	{
+		second_half = slow->next;
+		slow->next = NULL;
+	}
+	else
+	second_half = slow;
+	while (second_half)
+	{
+		temp = second_half->next;
+		second_half->next = prev;
+		prev = second_half;
+		second_half = temp;
+	}
+	second_half = prev;
 
-    // Find the middle of the list using slow and fast pointers
-    while (fast && fast->next)
-    {
-        fast = fast->next->next;
-        temp = slow;
-        slow = slow->next;
-    }
-
-    // If the list has an odd number of elements, ignore the middle element
-    if (fast)
-    {
-        second_half = slow->next;
-        slow->next = NULL;
-    }
-    else
-    {
-        second_half = slow;
-    }
-
-    // Reverse the second half of the list
-    while (second_half)
-    {
-        temp = second_half->next;
-        second_half->next = prev;
-        prev = second_half;
-        second_half = temp;
-    }
-
-    second_half = prev; // New head of the reversed second half
-
-    // Compare the first half and reversed second half
-    while (second_half)
-    {
-        if ((*head)->n != second_half->n)
-        {
-            is_palindrome = 0;
-            break;
-        }
-        *head = (*head)->next;
-        second_half = second_half->next;
-    }
-
-    return (is_palindrome);
+	while (second_half)
+	{
+		if ((*head)->n != second_half->n)
+		{
+			is_palindrome = 0;
+			break;
+		}
+		*head = (*head)->next;
+		second_half = second_half->next;
+	}
+	return (is_palindrome);
 }
-
