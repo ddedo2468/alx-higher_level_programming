db9a8185d9ee4e9e324294ee182358bfa207e492
Abdullah <abdallahmosbah25@gmail.com>
2023-09-13 13:09:53 -0700

    comit

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-def search(my_list, search, replace):
+def search_replace(my_list, search, replace):
+
     newlist = list()
     for element in my_list:
+
         if element == search:
             newlist.append(replace)
         else:
             newlist.append(element)
-    return newlist
+
+    return(newlist)
 
