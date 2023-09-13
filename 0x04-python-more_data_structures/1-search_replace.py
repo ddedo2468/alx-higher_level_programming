f56cdee0ba1880d02f606a69324a857b8e05ad8c
Abdullah <abdallahmosbah25@gmail.com>
2023-09-13 15:07:37 -0700

    1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    """
+    a function that search for enelmint in array and change it
+    with another element parameter
+    """
 
     newlist = []
     for element in my_list:
