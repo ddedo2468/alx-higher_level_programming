2e1f8d09e5f450b1f7ef064817190cbc5dedf3bc
Abdullah <abdallahmosbah25@gmail.com>
2023-09-13 14:56:12 -0700

    del lines

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,13 +2,10 @@
 def search_replace(my_list, search, replace):
 
     newlist = []
-
     for element in my_list:
-
         if element == search:
             newlist.append(replace)
         else:
             newlist.append(element)
-
     return newlist
 
