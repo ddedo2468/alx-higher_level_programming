54b97c4bf02bc66e5aee4f11c1a98d16a92e8e19
Abdullah <abdallahmosbah25@gmail.com>
2023-10-03 11:22:59 -0700

    i tryied

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""This is the "Matrix Divided" module """
+
+
+def matrix_divided(matrix, div):
+    """Return a new matrix with all values divided"""
+    if not isinstance(matrix, list):
+        raise TypeError("matrix must be a matrix (list of lists)"
+                        " of integers/floats")
+    if len(matrix) is 0:
+        raise TypeError("matrix must be a matrix (list of lists)"
+                        " of integers/floats")
+    if not all(len(l) > 0 for l in matrix):
+        raise TypeError("matrix must be a matrix (list of lists)"
+                        " of integers/floats")
+
+    if not all(len(l) == len(matrix[0]) for l in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+
+    for l in matrix:
+        if not isinstance(l, list):
+            raise TypeError("matrix must be a matrix (list of lists)"
+                            " of integers/floats")
+        if not all(isinstance(x, (int, float)) for x in l):
+            raise TypeError("matrix must be a matrix (list of lists)"
+                            " of integers/floats")
+
+    if div is 0:
+        raise ZeroDivisionError("division by zero")
+    if isinstance(div, bool):
+        raise TypeError("div must be a number")
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+
+    new_matrix = []
+    for l in matrix:
+        new_matrix.append(list(map(lambda n: round(n / div, 2), l)))
+
+    return new_matrix
