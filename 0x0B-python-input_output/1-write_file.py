e593931e64e6599e011963efe5587af51306b76e
Abdullah <abdallahmosbah25@gmail.com>
2023-10-10 07:13:24 -0700

    all done

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module for write_file method.
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Method for write_file.
+
+    Args:
+        filename (str): The file name.
+        text (str): The text to write.
+    """
+    with open(filename, mode="w", encoding="utf-8") as fhand:
+        return fhand.write(text)
