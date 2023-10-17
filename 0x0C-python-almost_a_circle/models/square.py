732fbd535db407e8d7082ca3e8c01f5ed3c8cb9e
Abdullah <abdallahmosbah25@gmail.com>
2023-10-16 17:49:20 -0700

    square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-""" defines Square class """
-
-
+"""Defines a square class."""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """Represent a square."""
+
     def __init__(self, size, x=0, y=0, id=None):
-        """ constructor method """
+        """ constructor method"""
         super().__init__(size, size, x, y, id)
 
     @property
@@ -20,29 +20,51 @@ class Square(Rectangle):
         self.width = value
         self.height = value
 
+    def update(self, *args, **kwargs):
+        """ update the values of square """
+        if args and len(args) != 0:
+            ar = 0
+            for arg in args:
+                if ar == 0:
+                    if arg is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif ar == 1:
+                    self.size = arg
+                elif ar == 2:
+                    self.x = arg
+                elif ar == 3:
+                    self.y = arg
+                ar += 1
+
+        elif kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "id":
+                    if v is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = v
+                elif k == "size":
+                    self.size = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
+
+    def to_dictionary(self):
+        """Return the dictionary representation of the Square."""
+        return {
+            "id": self.id,
+            "size": self.width,
+            "x": self.x,
+            "y": self.y
+        }
+
     def __str__(self):
-        """ better string for the  Square """
+        """Return the print() and str() representation of a Square."""
         return "[Square] ({}) {}/{} - {}".format(
                 self.id,
                 self.x,
                 self.y,
                 self.width)
-
-    def update(self, *args, **kwargs):
-        """ update the values of square """
-        if args:
-            attrs = ['id', 'size', 'x', 'y']
-            for i in range(len(args)):
-                setattr(self, attrs[i], args[i])
-        else:
-            for key, value in kwargs.items():
-                if key in ['id', 'size', 'x', 'y']:
-                    setattr(self, key, value)
-
-    def to_dictionary(self):
-        """ return dictionary of Square """
-        return {
-                'id': self.id,
-                'size': self.width,
-                'x': self.x,
-                'y': self.y}
