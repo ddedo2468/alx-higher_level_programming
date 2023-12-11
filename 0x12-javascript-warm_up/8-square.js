e14097171b0ab911f8a67695b89e9b9833918a5b
Abdullah <abdallahmosbah25@gmail.com>
2023-12-11 13:26:21 -0800

    square pants

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const x = process.argv[2];
+
+if (isNaN(x) || !x) {
+    console.log('Missing size');
+} else {
+    for (let i = 0; i < x; i++) {
+        console.log('X'.repeat(+x));
+    }
+}
