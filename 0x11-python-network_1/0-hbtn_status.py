f7383f91f44c5f7fa041d4a75cb4f8116c4cc843
Abdullah <abdallahmosbah25@gmail.com>
2024-01-06 21:00:49 +0200

    Edit Task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Python Script that fetches an url usuing urlib"""
+""" Python Script that fetches an url usuing urlib """
 
 import urllib.request
 
@@ -7,9 +7,10 @@ import urllib.request
 if __name__ == "__main__":
 
     url = "https://alx-intranet.hbtn.io/status"
+
     with urllib.request.urlopen(url) as res:
         bodymsg = res.read()
-        print("Body response")
+        print("Body response:")
         print("\t- type: {}".format(type(bodymsg)))
         print("\t- content: {}".format(bodymsg))
         print("\t- utf8 content: {}".format(bodymsg.decode('utf-8')))
