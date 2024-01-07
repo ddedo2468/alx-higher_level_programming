ace44a927961ff1a7e156c1722f8caa7e86e3c42
Abdullah <abdallahmosbah25@gmail.com>
2024-01-07 18:41:00 +0200

    Task 10 Clean

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -8,6 +8,9 @@ from requests.auth import HTTPBasicAuth
 
 
 if __name__ == "__main__":
-    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
-    r = requests.get("https://api.github.com/user", auth=auth)
-    print(r.json().get("id"))
+
+    aus = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+
+    res = requests.get("https://api.github.com/user", auth=aus)
+
+    print(res.json().get("id"))
