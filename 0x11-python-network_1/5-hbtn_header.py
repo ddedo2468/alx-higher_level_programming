ac44c4867c131657c60e7af1e7789ed3ecb94fbb
Abdullah <abdallahmosbah25@gmail.com>
2024-01-07 18:23:49 +0200

    Tasks 5 --> 6

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" python script to fetch url """
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    res = requests.get(url)
+
+    print(res.headers.get("X-Request-Id"))
