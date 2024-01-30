dde7c2671718919a7c1dc4b7fe5ff072839b72d0
Abdullah <abdallahmosbah25@gmail.com>
2024-01-31 00:57:42 +0200

    rr

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,12 +5,11 @@ const request = require('request');
 const epNum = process.argv[2];
 const URL = `https://swapi-api.alx-tools.com/api/films/${epNum}`;
 
-request(URL, function(err, res, body) {
-
+request(URL, function (err, res, body) {
   if (err) throw err;
 
   if (res.statusCode === 200) {
     const resJson = JSON.parse(body);
-    console.log(resJson.title)
+    console.log(resJson.title);
   }
-})
+});
